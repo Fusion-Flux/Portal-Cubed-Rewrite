33bb210456a2933a808c1fd04cd662f7d641d45e
TropheusJ <jverrellijr@icloud.com>
2023-10-26 11:18:20 -0400

    change tabs a bit

--- src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedItems.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedItems.java
@@ -9,7 +9,6 @@ import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 public class PortalCubedItems {
 	public static final Item TEST_ITEM = REGISTRAR.items.create("test_item")
 			.settings(QuiltItemSettings::fireproof)
-			.group(PortalCubedTabs.TEST_TAB.key())
 			.build();
 
 	public static void init() {
--- src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedTabs.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedTabs.java
@@ -2,20 +2,37 @@ package io.github.fusionflux.portalcubed.content;
 
 import io.github.fusionflux.portalcubed.PortalCubed;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.function.Consumer;
 
 public class PortalCubedTabs {
-	public static final Holder.Reference<CreativeModeTab> TEST_TAB = Registry.registerForHolder(
-			BuiltInRegistries.CREATIVE_MODE_TAB,
-			ResourceKey.create(Registries.CREATIVE_MODE_TAB, PortalCubed.id("test_tab")),
-			FabricItemGroup.builder().title(Component.literal("AAAAAAAAAA")).build()
-	);
+	public static final ResourceKey<CreativeModeTab> ITEM_GROUP = create("main", builder -> {
+		builder.icon(() -> new ItemStack(Blocks.DIRT));
+		builder.displayItems((params, output) -> {
+			output.accept(Items.STONE);
+		});
+	});
+
+	private static ResourceKey<CreativeModeTab> create(String name, Consumer<CreativeModeTab.Builder> consumer) {
+		CreativeModeTab.Builder builder = FabricItemGroup.builder().title(
+				Component.translatable("portalcubed.itemGroup." + name)
+		);
+		consumer.accept(builder);
+		return Registry.registerForHolder(
+				BuiltInRegistries.CREATIVE_MODE_TAB,
+				ResourceKey.create(Registries.CREATIVE_MODE_TAB, PortalCubed.id(name)),
+				builder.build()
+		).key();
+	}
 
 	public static void init() {
 	}
