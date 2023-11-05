0e8ce9a2c0871a9335a2249dd93edb0b0d5bd8f2
TropheusJ <jverrellijr@icloud.com>
2023-11-05 05:27:25 -0500

    portal picking

--- src/main/java/io/github/fusionflux/portalcubed/content/portal/PortalPickResult.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/portal/PortalPickResult.java
@@ -1,12 +1,12 @@
 package io.github.fusionflux.portalcubed.content.portal;
 
-import org.jetbrains.annotations.NotNull;
-
 import net.minecraft.world.phys.Vec3;
 
-public record PortalPickResult(Vec3 start, Vec3 end, Vec3 hit, Portal portal) implements Comparable<PortalPickResult> {
-	@Override
-	public int compareTo(@NotNull PortalPickResult that) {
-		return Double.compare(this.hit.distanceTo(this.start), that.hit.distanceTo(that.start));
-	}
+import java.util.Comparator;
+
+public record PortalPickResult(Vec3 start, Vec3 teleportedEnd, Vec3 hitIn, Vec3 hitOut, Portal portal) {
+	public static final Comparator<PortalPickResult> CLOSEST_TO_START = Comparator.comparingDouble(
+			result -> result.hitIn.distanceTo(result.start)
+	);
+
 }
--- src/main/java/io/github/fusionflux/portalcubed/content/portal/manager/PortalManager.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/portal/manager/PortalManager.java
@@ -4,10 +4,15 @@ import io.github.fusionflux.portalcubed.content.portal.Portal;
 import io.github.fusionflux.portalcubed.content.portal.PortalPickResult;
 import io.github.fusionflux.portalcubed.content.portal.storage.PortalStorage;
 import io.github.fusionflux.portalcubed.content.portal.storage.SectionPortalStorage;
+import net.fabricmc.api.EnvType;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 import org.jetbrains.annotations.Nullable;
+import org.quiltmc.loader.api.minecraft.MinecraftQuiltLoader;
 
 import java.util.List;
 import java.util.Objects;
@@ -19,18 +24,35 @@ public abstract class PortalManager {
 		return storage.all();
 	}
 
+	public static PortalManager of(Level level) {
+		if (level instanceof ServerLevel serverLevel) {
+			return ServerPortalManager.of(serverLevel);
+		} else if (MinecraftQuiltLoader.getEnvironmentType() == EnvType.CLIENT && level instanceof ClientLevel clientLevel) {
+			return ClientPortalManager.of(clientLevel);
+		}
+		throw new IllegalArgumentException(level + " is not ServerLevel or ClientLevel");
+	}
+
 	@Nullable
 	public PortalPickResult pickPortal(Vec3 start, Vec3 end) {
 		return storage.findPortalsInBox(new AABB(start, end))
-				.map(portal -> this.pickPortal(portal, start, end))
-				.filter(Objects::nonNull)
-				.sorted().findFirst().orElse(null);
+                .map(portal -> this.pickPortal(portal, start, end))
+                .filter(Objects::nonNull)
+				.min(PortalPickResult.CLOSEST_TO_START)
+				.orElse(null);
 	}
 
 	@Nullable
 	private PortalPickResult pickPortal(Portal portal, Vec3 start, Vec3 end) {
-//		Vec3 hit = portal.plane.pick(start, end);
-//		return hit == null ? null : new PortalPickResult(start, end, hit, portal);
-		return null;
+		// portals cannot be interacted with from behind
+		Vec3 delta = end.subtract(start);
+		if (delta.dot(portal.normal) > 0)
+			return null;
+		return portal.plane.clip(start, end).map(hit -> {
+			// TODO: transform these across portals
+			Vec3 teleportedEnd = end;
+			Vec3 hitOut = hit;
+            return new PortalPickResult(start, end, hit, hit, portal);
+        }).orElse(null);
 	}
 }
