c70c19ef694059c45cae9683477972465f44a852
maximumpower55 <maximumpowermc@gmail.com>
2024-02-29 16:33:12 -0800

    Manmade 4th dimensonal horrors

--- src/main/java/io/github/fusionflux/portalcubed/content/cannon/screen/widget/ConstructPreviewWidget.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/cannon/screen/widget/ConstructPreviewWidget.java
@@ -1,7 +1,10 @@
 package io.github.fusionflux.portalcubed.content.cannon.screen.widget;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import org.joml.Matrix4f;
 
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 
 import io.github.fusionflux.portalcubed.content.cannon.screen.CannonDataHolder;
@@ -16,15 +19,33 @@ import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
 public class ConstructPreviewWidget extends AbstractWidget {
 	public static final Component MESSAGE = ConstructionCannonScreen.translate("construct_preview");
 
+	public static final Vec3 ORIGIN = new Vec3(1, .5, 1);
+
+	private float animRot = 0;
 	private final CannonDataHolder data;
 
 	public ConstructPreviewWidget(int size, CannonDataHolder data) {
 		super(0, 0, size, size, MESSAGE);
 		this.data = data;
+	}
+
+	private void prepareForBlockRendering(PoseStack matrices) {
+		matrices.pushPose();
+		matrices.mulPoseMatrix(new Matrix4f().scaling(1, -1, 1));
+		matrices.translate(0, -2, 0);
+		RenderSystem.enableDepthTest();
+		Lighting.setupFor3DItems();
+	}
+
+	private void cleanupFromBlockRendering(PoseStack matrices) {
+		matrices.popPose();
+		Lighting.setupFor3DItems();
 	}
 
 	@Override
@@ -34,19 +55,29 @@ public class ConstructPreviewWidget extends AbstractWidget {
 
 		// render construct
 		this.data.get().construct().map(ConstructManager.INSTANCE::getConstructSet).ifPresent(set -> {
+			var construct = set.getDefault();
 			PoseStack matrices = graphics.pose();
 			matrices.pushPose();
 			matrices.translate(this.getX(), this.getY(), 0);
 			matrices.scale(40, 40, 1);
+			var constructCenter = AABB.of(construct.getBounds(Rotation.NONE)).deflate(.5).getCenter();
+			var renderOffset = constructCenter.vectorTo(ORIGIN);
+			constructCenter = constructCenter.add(renderOffset);
+			prepareForBlockRendering(matrices);
+			matrices.translate(constructCenter.x, constructCenter.y, constructCenter.z);
+			matrices.mulPose(Axis.YP.rotationDegrees(animRot));
+			matrices.mulPose(Axis.XP.rotationDegrees(30));
+			animRot = (animRot + (delta * 2f)) % 360f;
+			matrices.translate(-constructCenter.x, -constructCenter.y, -constructCenter.z);
 			set.getDefault().getBlocks(Rotation.NONE).forEach((pos, info) -> {
 				matrices.pushPose();
-				matrices.translate(pos.getX(), pos.getY(), pos.getZ());
-				matrices.mulPose(Axis.YN.rotationDegrees(Minecraft.getInstance().player.tickCount));
+				matrices.translate(pos.getX() + renderOffset.x, pos.getY() + renderOffset.y, pos.getZ() + renderOffset.z);
 				Minecraft.getInstance().getBlockRenderer().renderSingleBlock(
-						info.state(), matrices, graphics.bufferSource(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY
+					info.state(), matrices, graphics.bufferSource(), LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY
 				);
 				matrices.popPose();
 			});
+			cleanupFromBlockRendering(matrices);
 			matrices.popPose();
 		});
 	}
