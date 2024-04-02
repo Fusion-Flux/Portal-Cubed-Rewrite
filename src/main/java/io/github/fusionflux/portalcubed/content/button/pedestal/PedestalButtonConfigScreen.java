621420c7a53ec53ab8c19063abac7be600feb87c
maximumpower55 <maximumpowermc@gmail.com>
2024-04-02 16:21:07 -0700

    button close e

--- src/main/java/io/github/fusionflux/portalcubed/content/button/pedestal/PedestalButtonConfigScreen.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/button/pedestal/PedestalButtonConfigScreen.java
@@ -222,6 +222,16 @@ public class PedestalButtonConfigScreen extends Screen {
 	}
 
 	@Override
+	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+		// close on E
+		if (this.minecraft != null && this.minecraft.options.keyInventory.matches(keyCode, scanCode)) {
+			this.onClose();
+			return true;
+		}
+		return super.keyPressed(keyCode, scanCode, modifiers);
+	}
+
+	@Override
 	public void onClose() {
 		super.onClose();
 		tick();
