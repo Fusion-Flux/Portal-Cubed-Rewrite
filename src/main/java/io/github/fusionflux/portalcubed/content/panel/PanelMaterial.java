56b850c99faa84b9886a7bcd6c7466c33d1e3949
maximumpower55 <maximumpowermc@gmail.com>
2024-04-03 13:35:25 -0700

    properties

--- src/main/java/io/github/fusionflux/portalcubed/content/panel/PanelMaterial.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/panel/PanelMaterial.java
@@ -2,29 +2,33 @@ package io.github.fusionflux.portalcubed.content.panel;
 
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.MapColor;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import static io.github.fusionflux.portalcubed.content.panel.PanelMaterial.Flags.*;
 
 public enum PanelMaterial {
-	PORTAL_1_WHITE(CHECKERED, NO_2x2),
-	DIRTY_PORTAL_1_WHITE(CHECKERED, NO_2x2),
-	PORTAL_1_METAL(JOINER),
-	DIRTY_PORTAL_1_METAL(JOINER),
-	WHITE(CHECKERED),
-	AGED_WHITE(CHECKERED),
-	PADDED_GRAY,
-	AGED_PADDED_GRAY,
-	SMOOTH_GRAY,
-	AGED_SMOOTH_GRAY,
-	OLD_AP_WHITE(CHECKERED, NO_1x2, NO_HALF),
-	OLD_AP_GREEN(NO_HALF),
-	OLD_AP_BLUE(NO_HALF);
+	PORTAL_1_WHITE(PanelMaterial::p1WhiteSettings, CHECKERED, NO_2x2),
+	DIRTY_PORTAL_1_WHITE(PanelMaterial::p1DirtyWhiteSettings, CHECKERED, NO_2x2),
+	PORTAL_1_METAL(PanelMaterial::p1MetalSettings, JOINER),
+	DIRTY_PORTAL_1_METAL(PanelMaterial::p1DirtyMetalSettings, JOINER),
+	WHITE(PanelMaterial::whiteSettings, CHECKERED),
+	AGED_WHITE(PanelMaterial::agedWhiteSettings, CHECKERED),
+	PADDED_GRAY(PanelMaterial::paddedGraySettings),
+	AGED_PADDED_GRAY(PanelMaterial::paddedGraySettings),
+	SMOOTH_GRAY(PanelMaterial::smoothGraySettings),
+	AGED_SMOOTH_GRAY(PanelMaterial::smoothGraySettings),
+	OLD_AP_WHITE(PanelMaterial::oldApWhiteSettings, CHECKERED, NO_1x2, NO_HALF),
+	OLD_AP_GREEN(PanelMaterial::oldApGreenSettings, NO_HALF),
+	OLD_AP_BLUE(PanelMaterial::oldApBlueSettings, NO_HALF);
 
 	// maybe we'll do something with this eventually, kept mostly as a record for now
 	// DFU? It has major issues.
@@ -38,11 +42,7 @@ public enum PanelMaterial {
 
 	private final QuiltBlockSettings settings;
 
-	PanelMaterial(Flags... flags) {
-		this(settings -> {}, flags);
-	}
-
-	PanelMaterial(Consumer<QuiltBlockSettings> settingsModifier, Flags... flags) {
+	PanelMaterial(Supplier<QuiltBlockSettings> settingsCreator, Flags... flags) {
 		this.name = this.name().toLowerCase(Locale.ROOT);
 
 		Set<Flags> set = Set.of(flags);
@@ -58,12 +58,55 @@ public enum PanelMaterial {
 				})
 				.toList();
 
-		this.settings = QuiltBlockSettings.create();
-		settingsModifier.accept(this.settings);
+		this.settings = settingsCreator.get();
 	}
 
 	public QuiltBlockSettings getSettings() {
 		return QuiltBlockSettings.copyOf(this.settings);
+	}
+
+	public static QuiltBlockSettings p1WhiteSettings() {
+		return QuiltBlockSettings.copyOf(Blocks.WHITE_CONCRETE).mapColor(MapColor.COLOR_LIGHT_GRAY);
+	}
+
+	public static QuiltBlockSettings p1DirtyWhiteSettings() {
+		return p1WhiteSettings().mapColor(MapColor.TERRACOTTA_WHITE);
+	}
+
+	public static QuiltBlockSettings p1MetalSettings() {
+		return QuiltBlockSettings.copyOf(Blocks.COPPER_BLOCK).mapColor(MapColor.TERRACOTTA_BROWN);
+	}
+
+	public static QuiltBlockSettings p1DirtyMetalSettings() {
+		return p1MetalSettings().mapColor(MapColor.TERRACOTTA_GREEN);
+	}
+
+	public static QuiltBlockSettings whiteSettings() {
+		return QuiltBlockSettings.copyOf(Blocks.STONE).mapColor(MapColor.WOOD).sounds(SoundType.CHERRY_WOOD);
+	}
+
+	public static QuiltBlockSettings agedWhiteSettings() {
+		return whiteSettings().mapColor(MapColor.GRASS);
+	}
+
+	public static QuiltBlockSettings paddedGraySettings() {
+		return QuiltBlockSettings.copyOf(Blocks.STONE).mapColor(MapColor.DEEPSLATE);
+	}
+
+	public static QuiltBlockSettings smoothGraySettings() {
+		return QuiltBlockSettings.copyOf(Blocks.STONE).mapColor(MapColor.TERRACOTTA_CYAN);
+	}
+
+	public static QuiltBlockSettings oldApWhiteSettings() {
+		return QuiltBlockSettings.copyOf(Blocks.OAK_PLANKS).mapColor(MapColor.SAND);
+	}
+
+	public static QuiltBlockSettings oldApGreenSettings() {
+		return QuiltBlockSettings.copyOf(Blocks.COPPER_BLOCK).mapColor(MapColor.TERRACOTTA_LIGHT_GREEN).sounds(SoundType.NETHERITE_BLOCK);
+	}
+
+	public static QuiltBlockSettings oldApBlueSettings() {
+		return oldApGreenSettings().mapColor(MapColor.COLOR_CYAN);
 	}
 
 	enum Flags {
