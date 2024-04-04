e39bbe5661c152ae2d90ba92db9a0f37b8d4cecc
maximumpower55 <maximumpowermc@gmail.com>
2024-04-03 17:22:50 -0700

    h the reboot no one likes

--- src/main/java/io/github/fusionflux/portalcubed/framework/block/SimpleMultifaceBlock.java
+++ src/main/java/io/github/fusionflux/portalcubed/framework/block/SimpleMultifaceBlock.java
@@ -4,7 +4,7 @@ import com.mojang.serialization.MapCodec;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.MultifaceBlock;
@@ -16,6 +16,9 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class SimpleMultifaceBlock extends MultifaceBlock implements SimpleWaterloggedBlock {
 	public static final MapCodec<SimpleMultifaceBlock> CODEC = simpleCodec(SimpleMultifaceBlock::new);
@@ -41,6 +44,16 @@ public class SimpleMultifaceBlock extends MultifaceBlock implements SimpleWaterl
 	}
 
 	@Override
+	public VoxelShape getCollisionShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
+	}
+
+	@Override
+	public VoxelShape getOcclusionShape(BlockState state, BlockGetter world, BlockPos pos) {
+		return Shapes.empty();
+	}
+
+	@Override
 	public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos neighborPos) {
 		if (state.getValue(WATERLOGGED))
 			world.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
@@ -51,11 +64,6 @@ public class SimpleMultifaceBlock extends MultifaceBlock implements SimpleWaterl
 	@Override
 	public FluidState getFluidState(BlockState state) {
 		return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-	}
-
-	@Override
-	public boolean canBeReplaced(BlockState state, BlockPlaceContext context) {
-		return !context.getItemInHand().is(asItem()) || super.canBeReplaced(state, context);
 	}
 
 	@Override
