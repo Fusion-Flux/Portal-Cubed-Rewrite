b17c4c45fbc228e330cf831092dcb414a2901340
maximumpower55 <maximumpowermc@gmail.com>
2024-04-04 14:35:40 -0700

    scrolling: materials tab

--- src/main/java/io/github/fusionflux/portalcubed/content/cannon/screen/tab/MaterialsTab.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/cannon/screen/tab/MaterialsTab.java
@@ -7,6 +7,7 @@ import io.github.fusionflux.portalcubed.framework.gui.layout.PanelLayout;
 import io.github.fusionflux.portalcubed.framework.gui.widget.ScrollbarWidget;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.tags.TagKey;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.Item;
 
 import java.util.Optional;
@@ -14,29 +15,31 @@ import java.util.Optional;
 public class MaterialsTab {
 	public static final int ROWS = 4;
 	public static final int COLUMNS = 6;
+	public static final int SIZE = COLUMNS * ROWS;
 	public static final int X_OFF = 15;
 	public static final int Y_OFF = 44;
 
 	public static void init(CannonSettingsHolder settings, PanelLayout layout, ScrollbarWidget scrollBar) {
-		int i = 0;
 		var slots = new GridLayout();
-		scrollBar.active = false;
-		for (TagKey<Item> tag : ConstructManager.INSTANCE.getMaterialsSorted()) {
-			MaterialSlotWidget slot = new MaterialSlotWidget(tag, () -> {
-				slots.visitWidgets(widget -> ((MaterialSlotWidget) widget).deselect());
-				settings.update(s -> s.withMaterial(tag));
-			});
+		var materials = ConstructManager.INSTANCE.getMaterialsSorted();
+		int rowCount = Mth.positiveCeilDiv(materials.size(), COLUMNS) - ROWS;
+		int i = -(COLUMNS * Math.max((int) ((scrollBar.scrollPos() * rowCount) + .5f), 0));
+		scrollBar.active = materials.size() >= SIZE;
+		for (TagKey<Item> tag : materials) {
+			if (i >= 0) {
+				MaterialSlotWidget slot = new MaterialSlotWidget(tag, () -> {
+					slots.visitWidgets(widget -> ((MaterialSlotWidget) widget).deselect());
+					settings.update(s -> s.withMaterial(tag));
+				});
 
-			Optional<TagKey<Item>> material = settings.get().material();
-			if (material.isPresent() && material.get() == tag) {
-				slot.select();
-			}
+				Optional<TagKey<Item>> material = settings.get().material();
+				if (material.isPresent() && material.get() == tag) {
+					slot.select();
+				}
 
-			slots.addChild(slot, i / COLUMNS, i % COLUMNS);
-			if (++i >= COLUMNS * ROWS) {
-				scrollBar.active = true;
-				break;
+				slots.addChild(slot, i / COLUMNS, i % COLUMNS);
 			}
+			if (++i >= SIZE) break;
 		}
 		layout.addChild(X_OFF, Y_OFF, slots);
 	}
