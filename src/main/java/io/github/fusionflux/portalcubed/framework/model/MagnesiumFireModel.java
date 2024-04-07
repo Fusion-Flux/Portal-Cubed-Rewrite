ba7da284165ddef81363427441cd7a07fb24634b
maximumpower55 <maximumpowermc@gmail.com>
2024-04-07 14:07:58 -0700

    visual only magnesium fire 2/2

--- src/main/java/io/github/fusionflux/portalcubed/framework/model/MagnesiumFireModel.java
+++ src/main/java/io/github/fusionflux/portalcubed/framework/model/MagnesiumFireModel.java
@@ -1,9 +1,9 @@
 package io.github.fusionflux.portalcubed.framework.model;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.function.Supplier;
-
-import com.google.common.collect.Iterables;
 
 import io.github.fusionflux.portalcubed.data.tags.PortalCubedBlockTags;
 import io.github.fusionflux.portalcubed.mixin.client.KeyValueConditionAccessor;
@@ -11,7 +11,6 @@ import io.github.fusionflux.portalcubed.mixin.client.OrConditionAccessor;
 import io.github.fusionflux.portalcubed.mixin.client.SelectorAccessor;
 import net.fabricmc.fabric.api.renderer.v1.model.ForwardingBakedModel;
 import net.fabricmc.fabric.api.renderer.v1.render.RenderContext;
-import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.multipart.Condition;
 import net.minecraft.client.renderer.block.model.multipart.Selector;
 import net.minecraft.client.resources.model.BakedModel;
@@ -23,36 +22,41 @@ import net.minecraft.world.level.block.state.BlockState;
 
 public class MagnesiumFireModel extends ForwardingBakedModel {
 	private final BakedModel magnesiumVariant;
-	private final Direction direction;
+	private final Collection<Direction> directions;
 
 	MagnesiumFireModel(BakedModel wrapped, BakedModel magnesiumVariant, Selector selector) {
 		this.wrapped = wrapped;
 		this.magnesiumVariant = magnesiumVariant;
-		this.direction = directionFromCondition(((SelectorAccessor) selector).getCondition());
+		this.directions = directionsFromCondition(((SelectorAccessor) selector).getCondition());
 	}
 
-	private static Direction directionFromCondition(Condition condition) {
-		if (condition instanceof KeyValueConditionAccessor keyValue) {
+	private static Collection<Direction> directionsFromCondition(Condition condition) {
+		if (condition instanceof KeyValueConditionAccessor keyValue && keyValue.getValue().equals("true")) {
 			var dir = Direction.byName(keyValue.getKey());
-			if (dir != null) return dir;
+			if (dir != null)
+				return Collections.singleton(dir);
 		} else if (condition instanceof OrConditionAccessor or) {
-			return directionFromCondition(Iterables.getFirst(or.getConditions(), null));
+			var directions = new ArrayList<Direction>();
+			for (var subCondition : or.getConditions()) {
+				directions.addAll(directionsFromCondition(subCondition));
+			}
+			return directions;
 		}
-		return Direction.DOWN;
+		return Collections.singleton(Direction.DOWN);
 	}
 
 	@Override
 	public void emitBlockQuads(BlockAndTintGetter blockView, BlockState state, BlockPos pos, Supplier<RandomSource> randomSupplier, RenderContext context) {
-		if (blockView.getBlockState(pos.relative(direction)).is(PortalCubedBlockTags.MAGNESIUM_FIRE_BASE_BLOCKS)) {
+		boolean onMagnesium = false;
+		for (var dir : directions) {
+			onMagnesium |= blockView.getBlockState(pos.relative(dir)).is(PortalCubedBlockTags.MAGNESIUM_FIRE_BASE_BLOCKS);
+		}
+
+		if (onMagnesium) {
 			magnesiumVariant.emitBlockQuads(blockView, state, pos, randomSupplier, context);
 		} else {
 			super.emitBlockQuads(blockView, state, pos, randomSupplier, context);
 		}
-	}
-
-	@Override
-	public List<BakedQuad> getQuads(BlockState blockState, Direction face, RandomSource rand) {
-		throw new UnsupportedOperationException("isVanillaAdapter is false! call emitBlockQuads/emitItemQuads!");
 	}
 
 	@Override
