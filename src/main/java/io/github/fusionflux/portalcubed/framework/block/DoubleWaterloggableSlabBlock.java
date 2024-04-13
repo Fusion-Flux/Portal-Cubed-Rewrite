c964f115d1d49a4d1d630fe618e828de25ca01cd
maximumpower55 <maximumpowermc@gmail.com>
2024-04-13 11:40:21 -0700

    comment some stuff

--- src/main/java/io/github/fusionflux/portalcubed/framework/block/DoubleWaterloggableSlabBlock.java
+++ src/main/java/io/github/fusionflux/portalcubed/framework/block/DoubleWaterloggableSlabBlock.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.block.state.properties.SlabType;
 
 import org.jetbrains.annotations.Nullable;
 
+// rest of logic handled in SlabBlockMixin
 public class DoubleWaterloggableSlabBlock extends SlabBlock {
 	public DoubleWaterloggableSlabBlock(Properties properties) {
 		super(properties);
@@ -19,6 +20,7 @@ public class DoubleWaterloggableSlabBlock extends SlabBlock {
 	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
 		BlockPos blockPos = ctx.getClickedPos();
 		BlockState originalState = ctx.getLevel().getBlockState(blockPos);
+		// vanilla forcefully sets waterlogged to false here
 		if (originalState.is(this))
 			return originalState.setValue(TYPE, SlabType.DOUBLE);
 		return super.getStateForPlacement(ctx);
