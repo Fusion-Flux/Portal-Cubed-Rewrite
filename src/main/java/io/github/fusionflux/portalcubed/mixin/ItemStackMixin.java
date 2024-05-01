9ac81e446351254b6969b8d32e7b6dbcc887161e
maximumpower55 <maximumpowermc@gmail.com>
2024-05-01 15:31:47 -0700

    don't worry, cave took care of it

--- src/main/java/io/github/fusionflux/portalcubed/mixin/ItemStackMixin.java
+++ src/main/java/io/github/fusionflux/portalcubed/mixin/ItemStackMixin.java
@@ -6,6 +6,7 @@ import io.github.fusionflux.portalcubed.data.tags.PortalCubedItemTags;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
@@ -24,6 +25,13 @@ public class ItemStackMixin {
 	@Inject(method = "getTooltipLines", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Multimap;entries()Ljava/util/Collection;"))
 	private void injectFallDamageImmunityTooltip(@Nullable Player player, TooltipFlag context, CallbackInfoReturnable<List<Component>> cir, @Local List<Component> lines, @Local EquipmentSlot equipmentSlot) {
 		if (equipmentSlot == EquipmentSlot.FEET && ((ItemStack) (Object) this).is(PortalCubedItemTags.ABSORB_FALL_DAMAGE))
-			lines.add(Component.translatable("attribute.name.generic.fall_damage_absorption").withStyle(ChatFormatting.BLUE));
+			lines.add(
+					Component.translatable(
+							"attribute.modifier.plus." + AttributeModifier.Operation.MULTIPLY_TOTAL.toValue(),
+							"100",
+							Component.translatable("attribute.name.generic.fall_damage_absorption")
+					)
+					.withStyle(ChatFormatting.BLUE)
+			);
 	}
 }
