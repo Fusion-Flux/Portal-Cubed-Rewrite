1624115b565ff372c94e2cf8994ed5228510f3c2
maximumpower55 <maximumpowermc@gmail.com>
2024-05-31 16:50:04 -0700

    forgot a @Unique

--- src/main/java/io/github/fusionflux/portalcubed/mixin/client/ParticleEngineMixin.java
+++ src/main/java/io/github/fusionflux/portalcubed/mixin/client/ParticleEngineMixin.java
@@ -20,6 +20,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -35,6 +36,7 @@ public class ParticleEngineMixin implements ParticleEngineExt {
 	@Mutable
 	private static List<ParticleRenderType> RENDER_ORDER;
 
+	@Unique
 	private DecalParticleLightCache decalParticleLightCache;
 
 	@Override
