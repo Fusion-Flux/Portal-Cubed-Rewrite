645a6497046a0d3eee1f4a3db695b47eea1ee998
TropheusJ <jverrellijr@icloud.com>
2024-07-05 03:05:04 -0400

    rework gametest stuff

---
+++ src/gametests/java/io/github/fusionflux/portalcubed_gametests/PortalCubedGameTests.java
@@ -0,0 +1,5 @@
+package io.github.fusionflux.portalcubed_gametests;
+
+public class PortalCubedGameTests {
+	public static final String ID = "portalcubed_gametests";
+}
--- src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/PropGameTests.java
+++ src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/PropGameTests.java
@@ -1,6 +1,6 @@
-package io.github.fusionflux.portalcubed_test.gametest;
+package io.github.fusionflux.portalcubed_gametests.gametests;
 
-import io.github.fusionflux.portalcubed_test.PortalCubedTestmod;
+import io.github.fusionflux.portalcubed_gametests.PortalCubedGameTests;
 import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
@@ -10,8 +10,10 @@ import net.minecraft.world.level.block.RedstoneLampBlock;
 
 import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
 
-public class ExampleModGameTest implements QuiltGameTest {
-	@GameTest(template = PortalCubedTestmod.ID + ":floor_button_cube")
+public class PropGameTests implements QuiltGameTest {
+	private static final String group = PortalCubedGameTests.ID + ":props/";
+
+	@GameTest(template = group + "floor_button_cube")
 	public void floorButtonCube(GameTestHelper helper) {
 		helper.setBlock(new BlockPos(2, 3, 0), Blocks.AIR);
 		helper.succeedWhen(() -> helper.assertBlockProperty(new BlockPos(0, 2, 0), RedstoneLampBlock.LIT, true));
