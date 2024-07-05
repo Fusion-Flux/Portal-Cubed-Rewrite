a92cf641c133939b6d71fd04247a71cfba5c3ab5
Carter <69820300+Cart3r1234@users.noreply.github.com>
2024-07-05 02:14:37 -0400

    Start work on gametests

--- src/testmod/java/io/github/fusionflux/portalcubed_test/gametest/ExampleModGameTest.java
+++ src/testmod/java/io/github/fusionflux/portalcubed_test/gametest/ExampleModGameTest.java
@@ -1,17 +1,19 @@
 package io.github.fusionflux.portalcubed_test.gametest;
 
 import io.github.fusionflux.portalcubed_test.PortalCubedTestmod;
+import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Blocks;
+
+import net.minecraft.world.level.block.RedstoneLampBlock;
 
 import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
 
 public class ExampleModGameTest implements QuiltGameTest {
-	@GameTest(template = PortalCubedTestmod.ID + ":minecart_test")
-	public void minecartTest(GameTestHelper helper) {
-		helper.setBlock(2, 2, 0, Blocks.REDSTONE_BLOCK);
-		helper.succeedWhenEntityPresent(EntityType.MINECART, 2, 2, 3);
+	@GameTest(template = PortalCubedTestmod.ID + ":floor_button_cube")
+	public void floorButtonCube(GameTestHelper helper) {
+		helper.setBlock(new BlockPos(2, 3, 0), Blocks.AIR);
+		helper.succeedWhen(() -> helper.assertBlockProperty(new BlockPos(0, 2, 0), RedstoneLampBlock.LIT, true));
 	}
 }
