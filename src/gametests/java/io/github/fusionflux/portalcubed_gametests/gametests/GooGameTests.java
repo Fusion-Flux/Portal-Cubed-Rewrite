f839927d928bd7e3e89e1612520ed47d7d9ff895
maximumpower55 <maximumpowermc@gmail.com>
2024-07-05 21:50:32 -0700

    h

--- src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/GooGameTests.java
+++ src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/GooGameTests.java
@@ -17,11 +17,9 @@ public class GooGameTests implements QuiltGameTest {
 		helper.pullLever(3, 3, 2);
 		helper.pullLever(1, 3, 2);
 
-		helper.runAfterDelay(20, () -> {
-			helper.succeedWhen(() -> {
-						helper.assertBlockNotPresent(Blocks.AIR, new BlockPos(3, 2, 1));
-						helper.assertBlockPresent(Blocks.AIR, new BlockPos(1, 2, 1));
-					}
-			);
-		});	}
+		helper.succeedWhen(() -> {
+			helper.assertBlockNotPresent(Blocks.AIR, new BlockPos(3, 2, 1));
+			helper.assertBlockPresent(Blocks.AIR, new BlockPos(1, 2, 1));
+		});
+	}
 }
