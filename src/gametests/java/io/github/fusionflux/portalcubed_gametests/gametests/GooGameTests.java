ba40cc586efe9e88ec0b5a6c2c90bb363022198a
Carter <69820300+Cart3r1234@users.noreply.github.com>
2024-07-06 20:51:37 -0400

    gametest for goo destroying items + loot table fixes

--- src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/GooGameTests.java
+++ src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/GooGameTests.java
@@ -6,6 +6,8 @@ import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.level.block.Blocks;
 
+import net.minecraft.world.level.block.RedstoneLampBlock;
+
 import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
 
 public class GooGameTests implements QuiltGameTest {
@@ -20,6 +22,18 @@ public class GooGameTests implements QuiltGameTest {
 		helper.succeedWhen(() -> {
 			helper.assertBlockNotPresent(Blocks.AIR, new BlockPos(3, 2, 1));
 			helper.assertBlockPresent(Blocks.AIR, new BlockPos(1, 2, 1));
+		});
+	}
+
+	//Test the goo immunity item tag
+	@GameTest(template = GROUP + "goo_destroy_item")
+	public void gooDestroyItem(GameTestHelper helper) {
+		helper.pullLever(3, 4, 3);
+		helper.pullLever(1, 4, 3);
+
+		helper.succeedWhen(() -> {
+			helper.assertBlockProperty(new BlockPos(3, 2, 0), RedstoneLampBlock.LIT, true);
+			helper.assertBlockProperty(new BlockPos(1, 2, 0), RedstoneLampBlock.LIT, false);
 		});
 	}
 }
