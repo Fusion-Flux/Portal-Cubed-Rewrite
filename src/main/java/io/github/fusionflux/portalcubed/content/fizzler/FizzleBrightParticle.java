3d57feacbfdb411dcffa7e743e74f8f2bf0c92e4
maximumpower55 <maximumpowermc@gmail.com>
2024-07-12 14:14:51 -0700

    Minor fizzle particle tweaks

--- src/main/java/io/github/fusionflux/portalcubed/content/fizzler/FizzleBrightParticle.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/fizzler/FizzleBrightParticle.java
@@ -19,20 +19,40 @@ import org.jetbrains.annotations.NotNull;
 
 public class FizzleBrightParticle extends TextureSheetParticle {
 	public static final double SPEED = 0.05;
-	public static final float START_PROGRESS = 3f/4f;
+	public static final Vec3 FADE_DIRECTION = new Vec3(0, 1, 0);
+	public static final double FADE_DIRECTION_SPEED = 0.15;
+	public static final float FADE_START_PROGRESS = 3f/4f;
+	public static final float SIZE = 0.2f;
+
+	private Vec3 direction;
 
 	protected FizzleBrightParticle(ClientLevel world, double x, double y, double z) {
 		super(world, x, y, z);
-		Vec3 velocity = new Vec3(Math.random() * 2d - 1d, Math.random() * 2d - 1d, Math.random() * 2d - 1d)
-				.normalize()
-				.scale(SPEED);
-		this.xd = velocity.x;
-		this.yd = velocity.y;
-		this.zd = velocity.z;
+		this.direction = new Vec3(
+				Math.random() * 2d - 1d,
+				Math.random() * 2d - 1d,
+				Math.random() * 2d - 1d
+		).normalize();
+		this.updateVelocity();
 		this.roll = (float) (Math.random() * Mth.TWO_PI);
 		this.oRoll = this.roll;
 		this.hasPhysics = false;
 		this.friction = 1f;
+	}
+
+	private void updateVelocity() {
+		this.xd = this.direction.x * SPEED;
+		this.yd = this.direction.y * SPEED;
+		this.zd = this.direction.z * SPEED;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		if (this.age >= FADE_START_PROGRESS * this.lifetime) {
+			this.direction = this.direction.lerp(FADE_DIRECTION, FADE_DIRECTION_SPEED);
+			this.updateVelocity();
+		}
 	}
 
 	@NotNull
@@ -43,9 +63,9 @@ public class FizzleBrightParticle extends TextureSheetParticle {
 
 	@Override
 	public void render(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
-		float fade = 1 - Math.min((Math.max(0, (Math.min((this.age + tickDelta) / this.lifetime, 1)) - START_PROGRESS) / (1 - START_PROGRESS)), 1);
+		float fade = 1 - Math.min((Math.max(0, (Math.min((this.age + tickDelta) / this.lifetime, 1)) - FADE_START_PROGRESS) / (1 - FADE_START_PROGRESS)), 1);
 		this.setAlpha(fade);
-		this.quadSize = fade * 0.2f;
+		this.quadSize = SIZE * fade;
 		super.render(vertexConsumer, camera, tickDelta);
 	}
 
