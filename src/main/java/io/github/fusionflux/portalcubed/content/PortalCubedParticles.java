7912f92b6253eb810de5eb1965fc3107f7632711
maximumpower55 <maximumpowermc@gmail.com>
2024-07-14 16:14:54 -0700

    remove unused stuff

--- src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedParticles.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedParticles.java
@@ -4,7 +4,6 @@ import io.github.fusionflux.portalcubed.content.fizzler.FizzleBrightAlternatePar
 import io.github.fusionflux.portalcubed.content.fizzler.FizzleBrightParticle;
 import io.github.fusionflux.portalcubed.content.fizzler.FizzleDarkParticle;
 import io.github.fusionflux.portalcubed.framework.particle.DecalParticle;
-import io.github.fusionflux.portalcubed.framework.particle.EnergySparkParticle;
 import net.minecraft.core.particles.SimpleParticleType;
 
 import static io.github.fusionflux.portalcubed.PortalCubed.REGISTRAR;
@@ -14,12 +13,9 @@ public class PortalCubedParticles {
 
 	public static final SimpleParticleType SCORCH = REGISTRAR.particles.simple("scorch", () -> () -> DecalParticle.ScorchProvider::new);
 
-	public static final SimpleParticleType ENERGY_SPARK = REGISTRAR.particles.simple("energy_spark", () -> () -> EnergySparkParticle.Provider::new);
-
 	public static final SimpleParticleType FIZZLE_BRIGHT = REGISTRAR.particles.simple("fizzle_bright", () -> () -> FizzleBrightParticle.Provider::new);
 	public static final SimpleParticleType FIZZLE_BRIGHT_ALTERNATE = REGISTRAR.particles.simple("fizzle_bright_alternate", () -> () -> FizzleBrightAlternateParticle.Provider::new);
 	public static final SimpleParticleType FIZZLE_DARK = REGISTRAR.particles.simple("fizzle_dark", () -> () -> FizzleDarkParticle.Provider::new);
-	public static final SimpleParticleType FIZZLE_FLASH = REGISTRAR.particles.simple("fizzle_flash", () -> () -> EnergySparkParticle.Provider::new);
 
 	public static void init() {
 	}
