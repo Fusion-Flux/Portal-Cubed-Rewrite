832bb15dcda71b87e22886080f71648564ad4f5d
Carter <69820300+Cart3r1234@users.noreply.github.com>
2024-07-31 10:03:47 -0400

    gametests for destroying container items with goo/fizzling

---
+++ src/gametests/java/io/github/fusionflux/portalcubed_gametests/gametests/FizzlingGameTests.java
@@ -0,0 +1,34 @@
+package io.github.fusionflux.portalcubed_gametests.gametests;
+
+import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
+
+import io.github.fusionflux.portalcubed_gametests.PortalCubedGameTests;
+import net.minecraft.core.BlockPos;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.level.block.RedstoneLampBlock;
+
+public class FizzlingGameTests implements QuiltGameTest {
+	private static final String GROUP = PortalCubedGameTests.ID + ":fizzling/";
+
+	//Test container items dropping their contents when fizzled
+	@GameTest(template = GROUP + "fizzle_container")
+	public void fizzleContainer(GameTestHelper helper) {
+		helper.pullLever(2, 5, 4);
+
+		helper.runAfterDelay(80, () -> {  //Wait for items to be fizzled before unlocking the collection hopper
+			helper.pullLever(2, 2, 3);
+
+			helper.succeedWhen(() -> {
+				helper.assertBlockProperty(new BlockPos(1, 2, 1), RedstoneLampBlock.LIT, true);
+				helper.assertBlockProperty(new BlockPos(3, 2, 1), RedstoneLampBlock.LIT, true);
+			});
+		});
+	}
+
+	//Test hoppers and certain entity types being unable to pick up fizzling items
+	  // waiting on a fix for this
+
+	//Test fizzling items emitting a vibration properly
+	  // waiting on a fix for this
+}
