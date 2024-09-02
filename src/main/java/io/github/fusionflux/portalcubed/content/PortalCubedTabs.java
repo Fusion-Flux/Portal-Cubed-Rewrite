34098b7c270296c0257595ef415aa792b997bda3
Carter <69820300+Cart3r1234@users.noreply.github.com>
2024-09-02 04:23:56 -0400

    small signage models + signage asset location changes - I'll deal with the blockstate tomorrow

--- src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedTabs.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedTabs.java
@@ -35,6 +35,7 @@ public class PortalCubedTabs {
 			output.accept(PortalCubedBlocks.CUBE_BUTTON_BLOCK);
 			output.accept(PortalCubedBlocks.OLD_AP_FLOOR_BUTTON_BLOCK);
 			output.accept(PortalCubedBlocks.LARGE_SIGNAGE);
+			output.accept(PortalCubedBlocks.AGED_LARGE_SIGNAGE);
 			output.accept(PortalCubedBlocks.PEDESTAL_BUTTON);
 			output.accept(PortalCubedBlocks.OLD_AP_PEDESTAL_BUTTON);
 			addProp(output, PropType.PORTAL_1_STORAGE_CUBE);
