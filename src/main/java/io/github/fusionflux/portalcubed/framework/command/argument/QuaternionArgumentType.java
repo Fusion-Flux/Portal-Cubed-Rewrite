8596186437ede77111c70f9e71ad0a234792d7be
TropheusJ <jverrellijr@icloud.com>
2024-12-19 12:38:40 -0500

    fix quaternion parsing

--- src/main/java/io/github/fusionflux/portalcubed/framework/command/argument/QuaternionArgumentType.java
+++ src/main/java/io/github/fusionflux/portalcubed/framework/command/argument/QuaternionArgumentType.java
@@ -1,14 +1,20 @@
 package io.github.fusionflux.portalcubed.framework.command.argument;
 
-import com.mojang.brigadier.StringReader;
-import com.mojang.brigadier.arguments.ArgumentType;
-
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-
 import org.joml.Quaternionf;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+
+import net.minecraft.network.chat.Component;
+
 public class QuaternionArgumentType implements ArgumentType<Quaternionf> {
+	public static final SimpleCommandExceptionType INCOMPLETE = new SimpleCommandExceptionType(
+			Component.translatable("parsing.portalcubed.quaternion.incomplete")
+	);
+
 	public static QuaternionArgumentType quaternion() {
 		return new QuaternionArgumentType();
 	}
@@ -19,10 +25,23 @@ public class QuaternionArgumentType implements ArgumentType<Quaternionf> {
 
 	@Override
 	public Quaternionf parse(StringReader reader) throws CommandSyntaxException {
+		int cursor = reader.getCursor();
 		float x = reader.readFloat();
-		float y = reader.readFloat();
-		float z = reader.readFloat();
-		float w = reader.readFloat();
-		return new Quaternionf(x, y, z, w);
+		if (reader.canRead() && reader.peek() == ' ') {
+			reader.skip();
+			float y = reader.readFloat();
+			if (reader.canRead() && reader.peek() == ' ') {
+				reader.skip();
+				float z = reader.readFloat();
+				if (reader.canRead() && reader.peek() == ' ') {
+					reader.skip();
+					float w = reader.readFloat();
+					return new Quaternionf(x, y, z, w);
+				}
+			}
+		}
+
+		reader.setCursor(cursor);
+		throw INCOMPLETE.createWithContext(reader);
 	}
 }
