eeda73f27693bb5fdb663bb756bbe7cd2f024509
TropheusJ <jverrellijr@icloud.com>
2025-01-09 18:29:35 -0500

    misery

--- src/main/java/io/github/fusionflux/portalcubed/mixin/client/LocalPlayerMixin.java
+++ src/main/java/io/github/fusionflux/portalcubed/mixin/client/LocalPlayerMixin.java
@@ -19,18 +19,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.authlib.GameProfile;
-
-import io.github.fusionflux.portalcubed.content.boots.SourcePhysics;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.DeathScreen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.player.LocalPlayer;
-
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LocalPlayer.class)
