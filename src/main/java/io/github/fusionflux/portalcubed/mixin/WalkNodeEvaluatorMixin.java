47c0e76d5d2fce330d19634bd735309b778e5024
maximumpower55 <maximumpowermc@gmail.com>
2025-01-11 18:57:44 -0800

    fix pathfinding through chamber doors

---
+++ src/main/java/io/github/fusionflux/portalcubed/mixin/WalkNodeEvaluatorMixin.java
@@ -0,0 +1,27 @@
+package io.github.fusionflux.portalcubed.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import io.github.fusionflux.portalcubed.content.door.ChamberDoorBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
+
+@Mixin(WalkNodeEvaluator.class)
+public class WalkNodeEvaluatorMixin {
+	@WrapOperation(
+			method = "getPathTypeFromState",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/block/state/BlockState;getValue(Lnet/minecraft/world/level/block/state/properties/Property;)Ljava/lang/Comparable;",
+					ordinal = 0
+			)
+	)
+	private static Comparable<Boolean> useIsOpen(BlockState instance, Property<Boolean> property, Operation<Comparable<Boolean>> original) {
+		return instance.getBlock() instanceof ChamberDoorBlock door ? door.isOpen(instance) : original.call(instance, property);
+	}
+}
