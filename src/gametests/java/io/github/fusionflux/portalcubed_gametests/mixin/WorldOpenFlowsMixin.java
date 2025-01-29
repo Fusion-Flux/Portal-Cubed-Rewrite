f453d5fd20cca47a320e4ce2168aea7506fe0895
maximumpower55 <maximumpowermc@gmail.com>
2025-01-29 00:10:50 -0800

    krilled

--- src/gametests/java/io/github/fusionflux/portalcubed_gametests/mixin/WorldOpenFlowsMixin.java
+++ src/gametests/java/io/github/fusionflux/portalcubed_gametests/mixin/WorldOpenFlowsMixin.java
@@ -1,23 +1,21 @@
 package io.github.fusionflux.portalcubed_gametests.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import com.llamalad7.mixinextras.expression.Definition;
-import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.serialization.Lifecycle;
-
-import net.minecraft.client.gui.screens.worldselection.WorldOpenFlows;
-
 @Mixin(WorldOpenFlows.class)
 public class WorldOpenFlowsMixin {
-	@Definition(id = "lifecycle", local = @Local(type = Lifecycle.class, argsOnly = true))
-	@Definition(id = "stable", method = "Lcom/mojang/serialization/Lifecycle;stable()Lcom/mojang/serialization/Lifecycle;")
-	@Expression("lifecycle == stable()")
-	@ModifyExpressionValue(method = "confirmWorldCreation", at = @At("MIXINEXTRAS:EXPRESSION"))
-	private static boolean hereBeNoDragons(boolean original) {
-		return true;
+	@ModifyExpressionValue(
+			method = "openWorldCheckWorldStemCompatibility",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/world/level/storage/WorldData;worldGenSettingsLifecycle()Lcom/mojang/serialization/Lifecycle;"
+			)
+	)
+	private Lifecycle hereBeNoDragons(Lifecycle original) {
+		return Lifecycle.stable();
 	}
 }
