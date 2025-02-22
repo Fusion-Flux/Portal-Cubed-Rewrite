90e7d17410fa90242ac2eded5e4879aa5d5890e4
maximumpower55 <maximumpowermc@gmail.com>
2025-02-21 17:20:45 -0800

    override culling bounding box

--- src/main/java/io/github/fusionflux/portalcubed/mixin/portals/client/EntityRendererMixin.java
+++ src/main/java/io/github/fusionflux/portalcubed/mixin/portals/client/EntityRendererMixin.java
@@ -4,18 +4,42 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
 import io.github.fusionflux.portalcubed.content.portal.sync.EntityState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.world.TickRateManager;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 
-/**
- * This is pretty much only handling shadows.
- * @see LevelRendererMixin
- */
 @Mixin(EntityRenderer.class)
 public class EntityRendererMixin {
+	@WrapMethod(method = "getBoundingBoxForCulling")
+	private AABB getBoundingBoxForCulling(Entity entity, Operation<AABB> original) {
+		if (entity.level() instanceof ClientLevel level) {
+			TickRateManager tickRateManager = level.tickRateManager();
+			float tickDelta = Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(!tickRateManager.isEntityFrozen(entity));
+			EntityState override = entity.getTeleportProgressTracker().getEntityStateOverride(tickDelta);
+			if (override != null) {
+				Vec3 position = entity.position();
+				return entity.getBoundingBox()
+						.move(-position.x, -position.y, -position.z)
+						.move(override.pos());
+			}
+		}
+
+		return original.call(entity);
+	}
+
+	/**
+	 * This is pretty much only handling shadows.
+	 * @see LevelRendererMixin
+	 */
 	@ModifyReturnValue(
 			method = "createRenderState(Lnet/minecraft/world/entity/Entity;F)Lnet/minecraft/client/renderer/entity/state/EntityRenderState;",
 			at = @At("RETURN")
