4a713e544b2a4e22dcad1d60bdeec8019ab05925
Carter <69820300+Cart3r1234@users.noreply.github.com>
2026-04-04 12:10:57 -0400

    You know what they say about all aperture technology...

--- src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedItems.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/PortalCubedItems.java
@@ -83,10 +83,13 @@ public class PortalCubedItems {
 			.build();
 
 	public static final Item SCREEN_ASSEMBLY = REGISTRAR.items.create("screen_assembly", Item::new)
+			.properties(Item.Properties::fireResistant)
 			.build();
 	public static final Item KINETIC_ASSEMBLY = REGISTRAR.items.create("kinetic_assembly", Item::new)
+			.properties(Item.Properties::fireResistant)
 			.build();
 	public static final Item EMITTER_ASSEMBLY = REGISTRAR.items.create("emitter_assembly", Item::new)
+			.properties(Item.Properties::fireResistant)
 			.build();
 
 	public static final ConstructionCannonItem CONSTRUCTION_CANNON = REGISTRAR.items.create("construction_cannon", ConstructionCannonItem::new)
