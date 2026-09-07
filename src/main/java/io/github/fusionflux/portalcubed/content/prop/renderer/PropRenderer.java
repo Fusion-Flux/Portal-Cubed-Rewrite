e7e3228a924b0bc940e3cfc57d98f9a48aa49786
maximumpower55 <62370320+maximumpower55@users.noreply.github.com>
2026-09-07 12:26:58 -0700

    krill props for now

--- src/main/java/io/github/fusionflux/portalcubed/content/prop/renderer/PropRenderer.java
+++ src/main/java/io/github/fusionflux/portalcubed/content/prop/renderer/PropRenderer.java
@@ -9,10 +9,7 @@ import io.github.fusionflux.portalcubed.content.prop.entity.Prop;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider.Context;
-import net.minecraft.client.renderer.item.ItemStackRenderState;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.world.item.ItemDisplayContext;
 
 public class PropRenderer extends EntityRenderer<Prop, PropRenderState> {
 	private static final int[] EMPTY_TINT_LAYERS = new int[0];
@@ -30,23 +27,23 @@ public class PropRenderer extends EntityRenderer<Prop, PropRenderState> {
 		matrices.rotate(Axis.YP.rotationDegrees(180 - state.yRot));
 		matrices.translate(0, Y_OFFSET, 0);
 		matrices.scale(2, 2, 2);
-		PropModelCache.MeshAndTransform[] layers = PropModelCache.INSTANCE.get(state);
-		for (PropModelCache.MeshAndTransform layer : layers) {
-			matrices.pushPose();
-			layer.applyTransform(matrices.last());
-			submitNodeCollector.submitItem(
-					matrices,
-					ItemDisplayContext.GROUND,
-					state.lightCoords,
-					OverlayTexture.NO_OVERLAY,
-					0,
-					EMPTY_TINT_LAYERS,
-					layer.quads(),
-					ItemStackRenderState.FoilType.NONE
-
-			);
-			matrices.popPose();
-		}
+//		PropModelCache.MeshAndTransform[] layers = PropModelCache.INSTANCE.get(state);
+//		for (PropModelCache.MeshAndTransform layer : layers) {
+//			matrices.pushPose();
+//			layer.applyTransform(matrices.last());
+//			submitNodeCollector.submitItem(
+//					matrices,
+//					ItemDisplayContext.GROUND,
+//					state.lightCoords,
+//					OverlayTexture.NO_OVERLAY,
+//					0,
+//					EMPTY_TINT_LAYERS,
+//					layer.quads(),
+//					ItemStackRenderState.FoilType.NONE
+//
+//			);
+//			matrices.popPose();
+//		}
 		matrices.popPose();
 	}
 
